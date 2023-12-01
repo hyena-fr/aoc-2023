b4af5f72efeabbe53e5019f42228105321160a9f
hyena-fr <152598109+hyena-fr@users.noreply.github.com>
2023-12-01 10:05:11 +0000

    Clean code and Refactor

--- day1.py
+++ day1.py
@@ -1,41 +1,31 @@
 #Part 1
 with open('input.txt', 'r') as file:
-    sum = 0
+    total_sum = 0
     for line in file:
-        nums = ''
-        for char in line:
-            if char in '0123456789': nums += char
-        sum += int(nums[0] + nums[len(nums)-1])
-    print('Part 1: ', sum)
+        extracted_nums = ''.join(filter(str.isdigit, line))
+        if extracted_nums:
+            total_sum += int(extracted_nums[0]) + int(extracted_nums[-1])
+    print('Part 1:', total_sum)
 
 #Part 2
-hm = {
-    'zero': '0',
-    'one': '1',
-    'two': '2',
-    'three': '3',
-    'four': '4',
-    'five': '5',
-    'six': '6',
-    'seven': '7',
-    'eight': '8',
-    'nine': '9'
+num_mapping = {
+    'zero': '0', 'one': '1', 'two': '2', 'three': '3', 'four': '4',
+    'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'
 }
 
 with open('input.txt', 'r') as file:
-    sum = 0
+    total_sum = 0
     for line in file:
-        currStr = ''
-        nums = ''
-        for char in range(0, len(line)):
-            currStr += line[char]
-            if line[char] in '0123456789':
-                nums += line[char]
-                currStr = ''
+        extracted_nums = ''
+        current_word = ''
+        for char in line:
+            if char.isalpha():
+                current_word += char
+                if current_word in num_mapping:
+                    extracted_nums += num_mapping[current_word]
+                    current_word = ''
             else:
-                for i in ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'zero']:
-                    if i in currStr:
-                        nums += hm[i]
-                        currStr = currStr[len(currStr)-len(i):]
-        sum += int(nums[0] + nums[len(nums)-1])
-    print('Part 2: ', sum)
+                current_word = ''
+        if extracted_nums:
+            total_sum += int(extracted_nums[0]) + int(extracted_nums[-1])
+    print('Part 2:', total_sum)
